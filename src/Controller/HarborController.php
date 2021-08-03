05e956f1a6da73620a70e26efc96bc6f7dcfbdec
Darius Valancauskas <darius.veracity@gmail.com>
2021-08-03 17:49:24 +0300

    Harbors info by id

--- src/Controller/HarborController.php
+++ src/Controller/HarborController.php
@@ -4,7 +4,8 @@ namespace App\Controller;
 
 use App\Service\Harbors;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HarborController extends AbstractController
@@ -12,10 +13,24 @@ class HarborController extends AbstractController
     /**
      * @Route("/harbor", name="harbors_list")
      */
-    public function harborsList(Harbors $harbors): Response
+    public function harborsList(Harbors $harbors): JsonResponse
     {
         $list = $harbors->harborsList();
 
         return $this->json($list);
+    }
+
+    /**
+     * @Route("/harbor/{harborId}", name="harbors_list")
+     */
+    public function harborWeather(string $harborId, Harbors $harbors): JsonResponse
+    {
+        try {
+            $harbor = $harbors->getHarborWeather($harborId);
+        } catch (NotFoundHttpException $e) {
+            return $this->json(['error' => 'Harbor was not found by id']);
+        }
+
+        return $this->json($harbor);
     }
 }
--- src/Service/Harbors.php
+++ src/Service/Harbors.php
@@ -2,8 +2,13 @@
 
 namespace App\Service;
 
+use JetBrains\PhpStorm\ArrayShape;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 class Harbors
 {
+    private const WEATHER_PROVIDER_DOMAIN = 'https://api.openweathermap.org';
+
     private ApiClient $apiClient;
 
     public function __construct(ApiClient $apiClient)
@@ -13,9 +18,7 @@ class Harbors
 
     public function harborsList(): array
     {
-        $harborsJson = $this->apiClient->fetch('https://devapi.harba.co/harbors/visible');
-
-        $harborsFromApi = json_decode($harborsJson, true);
+        $harborsFromApi = $this->fetchHarbors();
 
         $harbors = [];
 
@@ -32,5 +35,43 @@ class Harbors
         }
 
         return $harbors;
+    }
+
+    #[ArrayShape(['name' => "string", 'weather' => "array", 'weather_provider' => "string"])]
+    public function getHarborWeather(string $id): array
+    {
+        $harbors = $this->fetchHarbors();
+
+        $harbor = null;
+
+        foreach ($harbors as $harborFromApi) {
+            if ($harborFromApi['id'] === $id) {
+                $harbor = $harborFromApi;
+                break;
+            }
+        }
+
+        if (null === $harbor) {
+            throw new NotFoundHttpException('Harbor not found by id');
+        }
+
+        $weather = $this->apiClient->fetch(sprintf(
+            self::WEATHER_PROVIDER_DOMAIN . '/data/2.5/weather?lat=%d&lon=%d&appid=b4b6cf42d49ae20d5a439454b8e19c1c',
+            $harbor['lat'],
+            $harbor['lon'],
+        ));
+
+        return [
+            'name' => $harbor['name'],
+            'weather' => json_decode($weather, true) ,
+            'weather_provider' => self::WEATHER_PROVIDER_DOMAIN,
+        ];
+    }
+
+    private function fetchHarbors(): array
+    {
+        $harborsJson = $this->apiClient->fetch('https://devapi.harba.co/harbors/visible');
+
+        return json_decode($harborsJson, true);
     }
 }
