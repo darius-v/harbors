54010f462a994c95eb05fa4acb00ac662d548e6b
Darius Valancauskas <darius.veracity@gmail.com>
2021-08-04 10:29:11 +0300

    Handling unexpected errors

--- src/Controller/HarborController.php
+++ src/Controller/HarborController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Throwable;
 
 class HarborController extends AbstractController
 {
@@ -15,22 +16,33 @@ class HarborController extends AbstractController
      */
     public function harborsList(Harbors $harbors): JsonResponse
     {
-        $list = $harbors->harborsList();
+        try {
+            $list = $harbors->harborsList();
+        } catch (Throwable $e) {
+            return $this->error('Unexpected error');
+        }
 
         return $this->json($list);
     }
 
     /**
-     * @Route("/harbor/{harborId}", name="harbors_list")
+     * @Route("/harbor/{harborId}", name="harbor_weather")
      */
     public function harborWeather(string $harborId, Harbors $harbors): JsonResponse
     {
         try {
             $harbor = $harbors->getHarborWeather($harborId);
         } catch (NotFoundHttpException $e) {
-            return $this->json(['error' => 'Harbor was not found by id']);
+            return $this->error('Harbor was not found by id');
+        } catch (Throwable $e) {
+            return $this->error('Unexpected error');
         }
 
         return $this->json($harbor);
+    }
+
+    private function error(string $error): JsonResponse
+    {
+        return $this->json(['error' => $error]);
     }
 }
